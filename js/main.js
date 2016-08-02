e54162ff7fce43bd9e18b21b2e1b7c1269ff03b4
Julio Aenlle <gmg8r251@ufl.edu>
2016-08-02 02:28:08 -0400

    Add custom javascript to main js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function () {
-    //
+
+    function addh2() {
+        var newtext = document.createElement('h2');
+        newtext.innerHTML = 'Good Food &amp; Good Gun, With Flair!';
+        document.getElementById('homeh1').appendChild(newtext);
+        homeh1.removeEventListener('mouseenter', addh2); //removes event listener
+    }
+
+    homeh1.addEventListener('mouseenter', addh2); //Event listener adds the Chotchkie's tagline whenever the mouse enter the homepage h1 element
 });
