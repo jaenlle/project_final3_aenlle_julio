78f319656bf1e734cde77273708b25dd2cc569be
Julio Aenlle <gmg8r251@ufl.edu>
2016-08-02 14:56:40 -0400

    Add plugin #1 to project
    Added SlipHover plugin elements to homepage and main JS file, and
    added styling to main CSS file.

--- js/main.js
+++ js/main.js
@@ -3,15 +3,16 @@ $(document).ready(function () {
     // Custom Javascript: adds the Chotchkie's tagline whenever the mouse enter the homepage h1 element
     function addh2() {
         var newtext = document.createElement('h2');
-        newtext.innerHTML = 'Good Food &amp; Good Gun, With Flair!';
+        newtext.innerHTML = 'Good Food &amp; Good Fun, With Flair!';
         document.getElementById('homeh1').appendChild(newtext);
         homeh1.removeEventListener('mouseenter', addh2); //removes event listener
     }
 
     homeh1.addEventListener('mouseenter', addh2);
 
-    // Plugin # 1:
-    $("#container").sliphover({
+    // Plugin # 1: The plugin's name is SlipHover. It creates a hover overlay that is dependent on the direction in which the mouse enters the image area.
+    $('#homecontent').sliphover({
+        backgroundColor: 'rgba(200,30,33,.7)',
 
     });
 
