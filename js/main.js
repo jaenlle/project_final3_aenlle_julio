6228d4ebd18c831f80ac6ee2ab9250a30e9f9041
Julio Aenlle <gmg8r251@ufl.edu>
2016-08-02 13:06:19 -0400

    Clean up JS file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // Custom Javascript: adds the Chotchkie's tagline whenever the mouse enter the homepage h1 element
     function addh2() {
         var newtext = document.createElement('h2');
         newtext.innerHTML = 'Good Food &amp; Good Gun, With Flair!';
@@ -7,5 +8,11 @@ $(document).ready(function () {
         homeh1.removeEventListener('mouseenter', addh2); //removes event listener
     }
 
-    homeh1.addEventListener('mouseenter', addh2); //Event listener adds the Chotchkie's tagline whenever the mouse enter the homepage h1 element
+    homeh1.addEventListener('mouseenter', addh2);
+
+    // Plugin # 1:
+
+
+
+    // Plugin # 2:
 });
