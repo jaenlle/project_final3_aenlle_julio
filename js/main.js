d745cb042ae3835e972d1a85d17c14b113c9f5c0
Julio Aenlle <gmg8r251@ufl.edu>
2016-08-04 19:31:11 -0400

    Move second plugin out of dom ready function

--- js/main.js
+++ js/main.js
@@ -12,14 +12,14 @@ $(document).ready(function () {
 
     homeh1.addEventListener('mouseenter', addh2);
 
-    // Plugin # 2:
-
     // Plugin # 1: The plugin's name is SlipHover. It creates a hover overlay that is dependent on the direction in which the mouse enters the image area.
     $('#homecontent').sliphover({
         backgroundColor: 'rgba(200,30,33,.7)'
     });
 });
 
+
+// Plugin # 2:
 $('.textillate').textillate({
     autostart: true,
     in: { effect: 'bounce', shuffle: false },
