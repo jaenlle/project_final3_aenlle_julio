59d8b4225d920d5e7f90fe30b65c7627969f1b7f
Julio Aenlle <gmg8r251@ufl.edu>
2016-08-04 19:52:51 -0400

    Update JS comments

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    // Custom Javascript: adds the Chotchkie's tagline whenever the mouse enter the homepage h1 element
+    // Custom Javascript: The custom javascript uses document.getElementById to pick out the h1 tag on the hompage, .createElement to create a new h2, innerHTML to add the new h2 content and finally .appendChild to add the new h2 to the document. The event listener is mouseenter and is added to the id "homeh1". I also used .removeEventListener to stop the addition of the h2 after just one mouse enter event.
     var homeh1 = document.getElementById('homeh1');
 
     function addh2() {
@@ -12,14 +12,14 @@ $(document).ready(function () {
 
     homeh1.addEventListener('mouseenter', addh2);
 
-    // Plugin # 1: The plugin's name is SlipHover. It creates a hover overlay that is dependent on the direction in which the mouse enters the image area.
+    // Plugin # 1: SlipHover is a jQuery that provides an image hover effect that cover images with an opaque layer and text. I used it on the homepage images.
     $('#homecontent').sliphover({
         backgroundColor: 'rgba(200,30,33,.7)'
     });
 });
 
 
-// Plugin # 2:
+// Plugin # 2: Textillate is a combination of lettering.js and automate.css. It uses lettering.js to split words or blocks of text into separate spans for each letter, and then uses animate.css to animate each letter (automate.css can only animate whole words). I applied it to the h1 tags for the menu, giftcard, and location pages.
 $('.textillate').textillate({
     autostart: true,
     in: { effect: 'bounce', shuffle: false },
