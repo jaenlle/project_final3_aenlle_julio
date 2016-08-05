cb204a80320d24cce894abe6e4726f3eacbaab13
Julio Aenlle <gmg8r251@ufl.edu>
2016-08-05 08:57:29 -0400

    Move custom jQuery hover over custom Javascript
    This fixed the issue of the custom jQuery hover states
    not showing up on pages other than the homepage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,42 @@
 $(document).ready(function () {
 
+    //Custom jQuery hover effect for the social media icons in the footer and on the location page
+    $('.twitter').hover(function () { //creates hover state for the footer twitter icon
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/twitter_green.png');
+    }, function () {
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/twitter_white.png');
+    });
+
+    $('.facebook').hover(function () { //creates hover state for the footer facebook icon
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/fb_green.png');
+    }, function () {
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/fb_white.png');
+    });
+
+    $('.instagram').hover(function () { //creates hover state for the footer instagram icon
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/ig_green.png');
+    }, function () {
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/ig_white.png');
+    });
+
+    $('.lo_twitter').hover(function () { //creates hover state for the location page twitter icon
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/twitter_green.png');
+    }, function () {
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/twitter_red.png');
+    });
+
+    $('.lo_facebook').hover(function () { //creates hover state for the location page facebook icon
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/fb_green.png');
+    }, function () {
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/fb_red.png');
+    });
+
+    $('.lo_instagram').hover(function () { //creates hover state for the location page instagram icon
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/ig_green.png');
+    }, function () {
+        $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/ig_red.png');
+    });
+
     // Custom Javascript: The custom javascript uses document.getElementById to pick out the h1 tag on the hompage, .createElement to create a new h2, innerHTML to add the new h2 content and finally .appendChild to add the new h2 to the document. The event listener is mouseenter and is added to the id "homeh1". I also used .removeEventListener to stop the addition of the h2 after just one mouse enter event.
     var homeh1 = document.getElementById('homeh1');
 
