011ade79a574e3dd36670ea14842f485f1b1dbf3
Julio Aenlle <gmg8r251@ufl.edu>
2016-08-05 13:03:07 -0400

    Create indiviual JS files for each page

--- js/location.js
+++ js/location.js
@@ -36,23 +36,6 @@ $(document).ready(function () {
     }, function () {
         $(this).attr('src', 'http://www.julioaenlle.net/project_final3_aenlle_julio/img/social/ig_red.png');
     });
-
-    // Custom Javascript: The custom javascript uses document.getElementById to pick out the h1 tag on the hompage, .createElement to create a new h2, innerHTML to add the new h2 content and finally .appendChild to add the new h2 to the document. The event listener is mouseenter and is added to the id "homeh1". I also used .removeEventListener to stop the addition of the h2 after just one mouse enter event.
-    var homeh1 = document.getElementById('homeh1');
-
-    function addh2() {
-        var newtext = document.createElement('h2');
-        newtext.innerHTML = 'Good Food &amp; Good Fun, With Flair!';
-        document.getElementById('homeh1').appendChild(newtext);
-        homeh1.removeEventListener('mouseenter', addh2); //removes event listener
-    }
-
-    homeh1.addEventListener('mouseenter', addh2);
-
-    // Plugin # 1: SlipHover is a jQuery that provides an image hover effect that cover images with an opaque layer and text. I used it on the homepage images.
-    $('#homecontent').sliphover({
-        backgroundColor: 'rgba(200,30,33,.7)'
-    });
 });
 
 
